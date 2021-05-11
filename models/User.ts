9761671ed1d09a46e815f82139b84ad0950626b6
Prince Abalogu <chineduabalogu@yahoo.com>
2021-05-11 12:14:03 +0100

    first commit

---
+++ models/User.ts
@@ -0,0 +1,47 @@
+const mongoose         = require('mongoose'),
+      Schema           = mongoose.Schema,
+      bcrypt           = require('bcrypt'),
+      SALT_WORK_FACTOR = 10
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add your Full Name']
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Please provide an email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password']
+  }
+})
+
+UserSchema.pre('save', (next) => {
+  
+  let user = this
+
+  if (!user.isModified('password')) return next()
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err)
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err)
+
+      user.password = hash
+      next()
+    })
+  })
+})
+
+UserSchema.methods.comparePassword = (userPassword, cb) => {
+  bcrypt.compare(userPassword, this.password, function(err, isMatch) {
+      if (err) return cb(err)
+      cb(null, isMatch)
+  })
+}
+
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema)
