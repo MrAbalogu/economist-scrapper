a0a96ed44fdcb88fb829e19b17ec559a42a49b02
Prince Abalogu <chineduabalogu@yahoo.com>
2021-05-14 11:02:11 +0100

    aws-lambda-chrome

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -7,7 +7,8 @@ async function dbConnect() {
 
   const db = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
   })
 
   connection.isConnected = db.connections[0].ready
