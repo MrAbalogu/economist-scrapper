c199eb21bb52a4e14ac71242224a99be40c83306
Prince Abalogu <chineduabalogu@yahoo.com>
2021-05-14 10:22:29 +0100

    test

--- pages/api/posts/scrapper.js
+++ pages/api/posts/scrapper.js
@@ -1,6 +1,6 @@
 import dbConnect from '@/utils/dbConnect.js'
 import Scrapper from '@/services/scrapper.js'
-import { parsePosts } from '@/controllers/PostsController.js'
+import { parsePosts } from '@/controllers/PostController.js'
 import { verifyCookie } from '@/controllers/AuthController.js'
 
 dbConnect()
