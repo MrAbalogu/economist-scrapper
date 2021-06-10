c6e0c9bb59eceae6266c4654cdeaf10932a2aa92
Prince Abalogu <chineduabalogu@yahoo.com>
2021-06-10 10:59:22 +0100

    test fixes

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -5,11 +5,13 @@ const connection = {}
 export async function dbConnect(url = process.env.MONGO_URI) {
   if(connection.isConnected) return
 
-  await mongoose.connect(url, {
+  const db = await mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
   })
+
+  console.log("connected db: ", db)
 }
 
 export async function dbDisconnect() {
