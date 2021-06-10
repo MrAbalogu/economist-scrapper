8c9aafe47b3bf72c68a68d8f43f7ba274cb647ee
Prince Abalogu <chineduabalogu@yahoo.com>
2021-06-10 10:55:30 +0100

    test fixes

--- __tests__/authenticate.test.js
+++ __tests__/authenticate.test.js
@@ -1,4 +1,4 @@
-import { dbConnect, closeDB } from "../utils/dbConnect"
+import { dbConnect, dbDisconnect } from "../utils/dbConnect"
 import User from '../models/User'
 import { signUp } from '../controllers/AuthController'
 import constants from "../utils/constants"
@@ -6,6 +6,12 @@ import constants from "../utils/constants"
 const URL = constants.TestDBUrl
 
 describe("Authentication", () => {
+  afterAll(async () => await dbDisconnect())
+
+  beforeEach(async () => await dbConnect(URL))
+
+  afterEach(async () => await User.deleteMany({}))
+
   let userParams = {
     name: "chinedu abalogu",
     email: "chineduabalogu@yahoo.com",
@@ -13,14 +19,6 @@ describe("Authentication", () => {
   }
 
   describe("Should Register User", () => {
-    beforeEach(async () => {
-      await dbConnect(URL)
-    })
-
-    afterEach(async () => {
-      await User.deleteMany({})
-    })
-
     it("Add row to mongoose user collection", async () => {
       const user = await signUp(userParams)
 
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -5,11 +5,13 @@ const connection = {}
 export async function dbConnect(url = process.env.MONGO_URI) {
   if(connection.isConnected) return
 
-  const db = await mongoose.connect(url, {
+  await mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
   })
+}
 
-  return db
+export async function dbDisconnect() {
+  await mongoose.disconnect()
 }
